737e8b028d78d76ca48a95de5cbdeeb0219a7d9c
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2024-04-03 22:03:32 -0300

    Primeiros passos

---
+++ Screen/dados/lista_info_dados_revendedora.js
@@ -0,0 +1,14 @@
+const localizacao = [
+    [' Av. Pedra Branca'],
+    ['231'],
+    ['Palhoça'],
+    ['SC'],
+    ['88137-280'],
+    ['https://maps.app.goo.gl/mWzyMYA2nogEthbb7']
+]
+const tel = [
+    ['48'],
+    ['99187-2598'],
+    ['https://wa.me/5548991872908']
+]
+const email = 'mailto:seemannguilherme@gmail.com'
